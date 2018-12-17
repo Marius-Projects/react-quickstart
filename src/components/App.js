0d67a92c22f743654178049275975cbac8065eef
Marius Mueller <marius.mueller@ge.com>
2018-12-17 14:35:10 -0600

    setup dev dockerfile

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Jenkins X Demo with React</h1>
+          <h1 className="App-title">Welcome to Jenkins X with React</h1>
         </header>
         <p className="App-intro">
-          I've made another edit!
+          Edit App.js to make changes
         </p>
       </div>
     );
